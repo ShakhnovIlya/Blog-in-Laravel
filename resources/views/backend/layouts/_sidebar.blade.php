082986556fc2fbfddd4c03f5369cd5159ea93486
IliyaShakhnou <ankordot@yandex.ru>
2023-12-24 20:31:34 +0300

    first commit

---
+++ resources/views/backend/layouts/_sidebar.blade.php
@@ -0,0 +1,49 @@
+  <aside id="sidebar" class="sidebar">
+
+    <ul class="sidebar-nav" id="sidebar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link " href="{{ url('panel/dashboard') }}">
+          <i class="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{ url('panel/user/list') }}">
+          <i class="bi bi-person"></i>
+          <span>Users</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{ url('panel/category/list') }}">
+          <i class="bi bi-person"></i>
+          <span>Category</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{ url('panel/blog/list') }}">
+          <i class="bi bi-person"></i>
+          <span>Blog</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="{{ url('panel/help/list') }}">
+          <i class="bi bi-question-circle"></i>
+          <span>Help</span>
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="">
+          <i class="bi bi-envelope"></i>
+          <span>Inbox</span>
+        </a>
+      </li>
+
+    </ul>
+
+  </aside>
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+Route::get('/', [HomeController::class, 'home']);
+
+Route::get('login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'auth_login']);
+
+Route::get('register', [AuthController::class, 'register']);
+Route::post('register', [AuthController::class, 'create_user']);
+Route::get('verify/{token}', [AuthController::class, 'verify']);
+
+Route::get('forgot-password', [AuthController::class, 'forgot']);
+Route::post('forgot-password', [AuthController::class, 'forgot_password']);
+
+Route::get('reset/{token}', [AuthController::class, 'reset']);
+Route::post('reset/{token}', [AuthController::class, 'post_reset']);
+
+Route::get('logout', [AuthController::class, 'logout']);
+
+Route::group(['middleware' => 'adminuser'], function ()
+{
+    Route::get('panel/dashboard', [DashboardController::class, 'dashboard']);
+});
