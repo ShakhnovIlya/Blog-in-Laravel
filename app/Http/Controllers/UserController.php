f6279fc3f5f38c60c40082b9571e091f69c83484
IliyaShakhnou <ankordot@yandex.ru>
2023-12-25 14:06:13 +0300

    working with functions Add, Edit, Delete Admin Panel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -12,5 +13,52 @@ class UserController extends Controller
     {
         $data['getRecord'] = User::getRecordUser();
         return view('backend.user.list', $data);
+    }
+
+    public function add_user(Request $request)
+    {
+        return view('backend.user.add');
+    }
+
+    public function insert_user(Request $request)
+    {
+        $save = new User;
+        $save->name = trim($request->name);
+        $save->email = trim($request->email);
+        $save->password = Hash::make($request->password);
+        $save->status = trim($request->status);
+        $save->save();
+
+        return redirect('panel/user/list')->with('success', 'User successfully created');
+    }
+
+    public function edit_user($id)
+    {
+        $data['getRecord'] = User::getSingle($id);
+        return view('backend.user.edit', $data);
+    }
+
+    public function update_user($id, Request $request)
+    {
+        $save = User::getSingle($id);
+        $save->name = trim($request->name);
+        $save->email = trim($request->email);
+
+        if(!empty($request->password))
+        {
+            $save->password = Hash::make($request->password);
+        }
+
+        $save->status = trim($request->status);
+        $save->save();
+
+        return redirect('panel/user/list')->with('success', 'User successfully updated');
+    }
+
+    public function delete_user($id)
+    {
+        $user = User::getSingle($id);
+        $user->delete();
+        return redirect('panel/user/list');
     }
 }
